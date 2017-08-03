24f0739bd3347b1ddc12dbf254d46b56c17058a8
lucahernandezacosta <l.hernandezacosta@stud.uni-goettingen.de>
2017-08-03 17:22:51 +0200

    Initial Commit

---
+++ HelpTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelpTheWorld
+//
+//  Created by Luca Hernández Acosta on 03.08.17.
+//  Copyright © 2017 Luca Hernández Acosta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
